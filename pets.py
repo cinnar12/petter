5b4127f1770324867cedb46fc168878b849027ca
Tho Tran <cinnar12@gmail.com>
2018-05-01 01:37:12 -0400

    flask1

---
+++ pets.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from modules import convert_to_dict, make_ordinal
+
+app = Flask(__name__)
+
+pet_list = convert_to_dict("pets.csv")
+
+@app.route('/')
+def index():
+    for pet in pet_list:
+            pet_dict = pet
+            break
+    return render_template('profile.html', pet=pet_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)
